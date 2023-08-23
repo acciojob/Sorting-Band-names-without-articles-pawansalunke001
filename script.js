359a5ba44158d57a1caf4a3c8ab164d3a85f5f09
Pawan Salunke <133101693+pawansalunke001@users.noreply.github.com>
2023-08-23 16:12:22 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 //your code here
+let bandNames = ['The Beatles', 'Green Day', 'The Rolling Stones', 'Aerosmith', 'Led Zeppelin'];
+
+// Remove articles ('a', 'an', 'the') from band names
+function removeArticles(name) {
+    return name.replace(/^(a |an |the )/i, '');
+}
+
+// Sort band names without articles in lexicographic order
+bandNames = bandNames.sort((a, b) => removeArticles(a).localeCompare(removeArticles(b)));
+
+// Get the ul element
+const ulElement = document.getElementById('band');
+
+// Clear any existing content
+ulElement.innerHTML = '';
+
+// Add sorted band names to the ul element
+for (const name of bandNames) {
+    const liElement = document.createElement('li');
+    liElement.textContent = name;
+    ulElement.appendChild(liElement);
+}
+
 
